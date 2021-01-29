2184b33caceff4d2e238e8421785be4a7781a8f9
Number1ava <avanumbo1@gmail.com>
2021-01-29 11:11:12 +0500

    update js

--- app/js/main.js
+++ app/js/main.js
@@ -24,7 +24,7 @@ burgerMenu(".burger__menu");
 
 $(document).ready(function () {
   let show = true;
-  let countbox = ".stats__top";
+  let countbox = ".reg__inner";
   $(window).on("scroll load resize", function () {
     if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
     let w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
@@ -32,6 +32,27 @@ $(document).ready(function () {
     let w_height = $(window).height(); // Высота окна браузера
     let d_height = $(document).height(); // Высота всего документа
     let e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+    if (w_top + 400 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+      $('.status__number-animated').css('opacity', '1');
+      $('.status__number-animated').spincrement({
+        thousandSeparator: "",
+        duration: 4800
+      });
+      show = false;
+    }
+  });
+});
+
+$(document).ready(function () {
+  let show = true;
+  let countbox = ".stats__top";
+  $(window).on("scroll load resize", function () {
+    if (!show) return false;
+    let w_top = $(window).scrollTop();
+    let e_top = $(countbox).offset().top;
+    let w_height = $(window).height();
+    let d_height = $(document).height();
+    let e_height = $(countbox).outerHeight();
     if (w_top + 400 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
       $('.stats__main-number').css('opacity', '1');
       $('.stats__main-number').spincrement({
